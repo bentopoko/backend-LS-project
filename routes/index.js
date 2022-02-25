134ee0ba7ef137726801ed05c9dcdbd0d2436a82
bento_poko <bpcool22@hotmail.com>
2022-02-25 17:01:50 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,54 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+// products list from db
+router.get("/products", function (req, res, next) {
+  res.json({});
+});
+
+// product from db by ID
+router.get("/products/:id", function (req, res, next) {
+  res.json({});
+});
+
+// sign-up
+router.post("/users/actions/sign-up", function (req, res, next) {
+  res.json({});
+});
+
+// sign-in
+router.post("/users/actions/sign-in", function (req, res, next) {
+  res.json({});
+});
+
+// order registration
+router.post("/orders", function (req, res, next) {
+  res.json({});
+});
+
+// orders history by user ID
+router.get("/orders/users/:id", function (req, res, next) {
+  res.json({});
+});
+
+// profile by user Id (slide 15)
+router.get("/users/:id", function (req, res, next) {
+  res.json({});
+});
+
+// profile update
+router.put("/users/update/:token", function (req, res, next) {
+  res.json({});
+});
+
+// profile delete by user Id (slide 15)
+router.delete("/users/delete/:token", function (req, res, next) {
+  res.json({});
+});
+
+module.exports = router;
---
+++ routes/index.test.js
@@ -0,0 +1,37 @@
+const { TestWatcher } = require("jest");
+const request = require("supertest");
+
+const app = require("../app");
+
+test("user token registration", async () => {
+  const response = await request(app)
+    .post("/users/actions/sign-up")
+
+    .send({
+      firstname: "john",
+      lastname: "doe",
+      pseudo: "wallace",
+      password: "abc",
+    });
+  expect(typeof response.body.token).toBe("string");
+});
+
+test("password_validation over 8 characters", async () => {
+  const response = await request(app)
+    .post("/users/actions/sign-up")
+
+    .send({
+      password: "2143256547565785jh",
+    });
+  expect(typeof response.body.token).toBe("string");
+});
+
+test("password_error below 8 characters", async () => {
+  const response = await request(app)
+    .post("/users/actions/sign-up")
+
+    .send({
+      password: "214325",
+    })
+    .expect({ result: false, err: "ups... invalid password" });
+});
