16f220f495867de98943dd36ae6b30ec44bcccc2
bento_poko <bpcool22@hotmail.com>
2022-02-28 17:26:45 +0100

    final product & order db connect

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,32 @@ var express = require("express");
 var router = express.Router();
 
 var userModel = require("../models/users");
+var orderModel = require("../models/orders");
+var productModel = require("../models/products");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
+});
+
+// add products into the database
+router.post("/products", async function (req, res, next) {
+  console.log("/products", req.body);
+
+  var newProduct = new productModel({
+    title: req.body.title,
+    description: req.body.description,
+    price: req.body.price,
+    img: req.body.img,
+    stock: req.body.stock,
+    category: req.body.category,
+  });
+  console.log("newProduct", newProduct);
+
+  var saveProduct = await newProduct.save();
+  console.log("saveProduct", saveProduct);
+
+  res.json({ saveProduct });
 });
 
 // products list from db
@@ -29,8 +51,39 @@ router.post("/users/actions/sign-in", function (req, res, next) {
 });
 
 // order registration
-router.post("/orders", function (req, res, next) {
-  res.json({});
+router.post("/orders", async function (req, res, next) {
+  console.log("/orders", req.body);
+
+  var userFind = await userModel.findOne({
+    token: req.body.token,
+  });
+  console.log("userFind", userFind);
+
+  var productFind = await productModel.find({
+    userToken: userFind.token,
+  });
+  console.log("userFind", userFind);
+
+  var newOrder = new orderModel({
+    product: [
+      {
+        productID: productFind._id,
+        qty: req.body.qty,
+      },
+    ],
+    clientToken: userFind.token,
+    dateInsert: req.body.date_insert,
+    statusPayment: req.body.status_payment,
+    datePayment: req.body.date_payment,
+    timePicker: req.body.time_picker,
+    statusDelivery: req.body.status_delivery,
+    statusPreparation: req.body.status_preparation,
+  });
+
+  var saveOrder = await newOrder.save();
+  console.log("saveOrder", saveOrder);
+
+  res.json({ saveOrder });
 });
 
 // orders history by user ID
